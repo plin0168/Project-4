2b7873e75bd87193cf99c09f1ad721f408c91c0c
Philip Lin <philipo227@hotmail.com>
2016-11-04 10:03:04 -0700

    Heroku adjustment

--- client/js/services/gameFactory.js
+++ client/js/services/gameFactory.js
@@ -33,7 +33,6 @@ function GameFactory($http){
 
   function getVideo(){
     var apiKey = process.env.API_KEY
-    // var apiURL = "https://www.googleapis.com/youtube/v3/channels?id=UCCWQ_UwBYZSHDUtkC1nTyXw&key=AIzaSyBEdo7-BuluP3i-xgKQxa6SombQ2pL0wF8&typesearch?videoEmbeddable=true&order=date&part=snippet&=video&maxResults=10"
     var apiURL = "https://www.googleapis.com/youtube/v3/channels?id=UCCWQ_UwBYZSHDUtkC1nTyXw&" + apiKey + "&videoEmbeddable=true&order=date&part=snippet&=video&maxResults=10"
     // https://www.googleapis.com/youtube/v3/channels?id=fjTOrCPnAblTngWAzpnlMA&key={YOUR_API_KEY}&part=snippet,contentDetails,statistics
     return $http.get(apiURL);
